2657160ea8acb248a31c8d12748dbab43761a104
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-04-24 17:04:16 +0100

    Update Image.hpp

--- project/Image.hpp
+++ project/Image.hpp
@@ -1,13 +1,17 @@
 #ifndef __prog_Image_hpp__
 #define __prog_Image_hpp__
 #include "Color.hpp"
+#include <vector>
 
 namespace prog
 {
   class Image
   {
   private:
-    // TODO: define private fields for image state
+    int w;
+    int h;
+    std::vector<int> pixels;
+
   public:
     Image(int w, int h, const Color &fill = {255, 255, 255});
     ~Image();

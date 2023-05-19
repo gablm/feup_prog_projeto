7dce2440f33e90b5ba36b0ba165f39e159a87e08
Troxcsmel <joaop.tedim@gmail.com>
2023-05-19 16:48:50 +0100

    Update Script.hpp

--- final_product/Script.hpp
+++ final_product/Script.hpp
@@ -1,49 +1,48 @@
 #ifndef __prog_Script_hpp__
 #define __prog_Script_hpp__
-
 #include <string>
 #include <fstream>
 #include "Image.hpp"
 
-namespace prog
-{
-  class Script
-  {
-  public: 
-    Script(const std::string &filename);
-    ~Script();
-    void run();
+namespace prog {
+  class Script {
+    public:
+      Script(const std::string &filename);
+      ~Script();
+      void run();
 
-  private:
-    // Current image.
-    Image *image;
-    // Input stream for reading script commands.
-    std::ifstream input;
+    //The image the functions will work on and the respective stream where the commands will be inserted
+    private:
+      // Current image
+      Image *image;
+      
+      // Input stream for reading script commands
+      std::ifstream input;
 
-  private:
-    // Private functions
-    void clear_image_if_any();
-    void open();
-    void blank();
-    void save();
+    private:
+      // Private functions
+      void clear_image_if_any();
+      void open();
+      void blank();
+      void save();
 
-    // Simple image manipulation
-    void invert();
-    void to_gray_scale();
-    void replace(int r1, int g1, int b1, int r2, int g2, int b2);
-    void fill(int x, int y, int w, int h, int r, int g, int b);
-    void h_mirror();
-    void v_mirror();
-    void add(std::string filename, int r, int g, int b, int x, int y);
+      // Simple image manipulation
+      void invert();
+      void to_gray_scale();
+      void replace(int r1, int g1, int b1, int r2, int g2, int b2);
+      void fill(int x, int y, int w, int h, int r, int g, int b);
+      void h_mirror();
+      void v_mirror();
+      void add(std::string filename, int r, int g, int b, int x, int y);
 
-    // Dimension-changing operations
-    void crop(int x, int y, int w, int h);
-    void rotate_left();
-    void rotate_right();
+      // Dimension-changing operations
+      void crop(int x, int y, int w, int h);
+      void rotate_left();
+      void rotate_right();
 
-    // Advanced funcionalities
-    void median_filter(int ws);
-    Color median_color(int ws, int x, int y);
+      // Advanced funcionalities
+      void median_filter(int ws);
+      Color median_color(int ws, int x, int y);
   };
 }
 #endif
